93400913d9212e3bac6bea1e6d365cca0acf54b8
Absdevelop <abnergmtomaz@gmail.com>
2022-08-24 13:55:02 -0300

    Update

--- app/Models/ProdutosModel.php
+++ app/Models/ProdutosModel.php
@@ -17,15 +17,4 @@ class ProdutosModel extends Model
     public function getProduto($id){
         return $this->asArray()->where(['id'=>$id])->first();
     }
-
-    public function getRegistros(){
-        $sql = $db->query('SELECT * FROM produtos LIMIT 3');
-        $results = $sql->getResult();
-
-        foreach ($results as $row) {
-            echo $row->title;
-            echo $row->name;
-            echo $row->email;
-        }
-    }
 }
